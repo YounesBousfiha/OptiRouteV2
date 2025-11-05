d804329ba4c0de5dfddacdde33870ab670d4c8a5
Coderyounes <younesanas2021@gmail.com>
2025-11-05 17:17:03 +0100

    CHORE: return back entrypoint

--- src/main/java/com/optiroute/optiroute/OptirouteApplication.java
+++ src/main/java/com/optiroute/optiroute/OptirouteApplication.java
@@ -1,14 +1,15 @@
 package com.optiroute.optiroute;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:applicationContext.xml")
 public class OptirouteApplication {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(OptirouteApplication.class, args);
 	}
 }
