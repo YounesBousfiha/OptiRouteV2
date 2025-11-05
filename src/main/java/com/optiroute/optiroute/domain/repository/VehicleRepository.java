988bfaac18e70077cfa3ed9ecc749fc3269af484
Coderyounes <younesanas2021@gmail.com>
2025-11-05 17:14:21 +0100

    FEAT : add @Repository annotation to VehicleRepository

--- src/main/java/com/optiroute/optiroute/domain/repository/VehicleRepository.java
+++ src/main/java/com/optiroute/optiroute/domain/repository/VehicleRepository.java
@@ -2,6 +2,9 @@ package com.optiroute.optiroute.domain.repository;
 
 import com.optiroute.optiroute.domain.entity.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface VehicleRepository  extends JpaRepository<Vehicule, Long> {
 }
