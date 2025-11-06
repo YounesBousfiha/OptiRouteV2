034f6e7d74daaee78e032de4959918afb0b52552
Coderyounes <younesanas2021@gmail.com>
2025-11-06 11:44:55 +0100

    FEAT: add @Repository annotation to CustomerRepository for Spring component scanning

--- src/main/java/com/optiroute/optiroute/domain/repository/CustomerRepository.java
+++ src/main/java/com/optiroute/optiroute/domain/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.optiroute.optiroute.domain.repository;
 
 import com.optiroute.optiroute.domain.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
