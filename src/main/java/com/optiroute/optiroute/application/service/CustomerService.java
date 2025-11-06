bfceb402c5e419c9ab6f4a779501db27055d9f38
Coderyounes <younesanas2021@gmail.com>
2025-11-06 12:28:58 +0100

    FEAT: implement customer management methods in CustomerService

--- src/main/java/com/optiroute/optiroute/application/service/CustomerService.java
+++ src/main/java/com/optiroute/optiroute/application/service/CustomerService.java
@@ -1,11 +1,24 @@
 package com.optiroute.optiroute.application.service;
 
 
+import com.optiroute.optiroute.application.mapper.CustomerMapper;
+import com.optiroute.optiroute.domain.entity.Customer;
+import com.optiroute.optiroute.domain.exception.DatabaseException;
 import com.optiroute.optiroute.domain.repository.CustomerRepository;
+import com.optiroute.optiroute.presentation.dto.request.CustomerRequestDTO;
+import com.optiroute.optiroute.presentation.dto.response.CustomerResponseDTO;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@Slf4j
 public class CustomerService {
 
     private CustomerRepository customerRepository;
@@ -14,4 +27,42 @@ public class CustomerService {
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
+
+    public List<CustomerResponseDTO> getAllCustomers() {
+        try {
+            return customerRepository.findAll().stream()
+                    .map(CustomerMapper::toDTO)
+                    .toList();
+        } catch (DataAccessException ex) {
+            log.error("Database Exception While Fetch All Customer: {}", ex.getMessage());
+            throw new DatabaseException("Failed to fetch customers", ex);
+        }
+    }
+
+    public CustomerResponseDTO createCustomer(@Valid CustomerRequestDTO dto) {
+        try {
+            Customer customer = CustomerMapper.toEntity(dto);
+
+            Customer newCustomer = this.customerRepository.save(customer);
+
+            return CustomerMapper.toDTO(newCustomer);
+        } catch (Exception ex) {
+            log.error("Exception while create Customer: {}", ex.getMessage());
+            throw new DatabaseException("Error while Creating Customer", ex);
+        }
+    }
+
+    public CustomerResponseDTO getOneCustomer(Long id) {
+        return null;
+    }
+
+    public CustomerResponseDTO updateCustomer(Long id) {
+        return null;
+    }
+
+    public boolean deleteCustomer(Long id) {
+        return true;
+    }
+
+
 }
