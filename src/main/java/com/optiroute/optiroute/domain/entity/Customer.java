52356df1cecf11621005c54ee486194cf9070e3f
Coderyounes <younesanas2021@gmail.com>
2025-11-06 01:22:46 +0100

    FEAT: add Customer entity and changelog for customer table creation

---
+++ src/main/java/com/optiroute/optiroute/domain/entity/Customer.java
@@ -0,0 +1,45 @@
+package com.optiroute.optiroute.domain.entity;
+
+
+import com.optiroute.optiroute.domain.vo.Address;
+import com.optiroute.optiroute.domain.vo.Coordinates;
+import com.optiroute.optiroute.domain.vo.PreferredTimeSlot;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    @AttributeOverride(name="latitude", column=@Column(name="lat"))
+    @AttributeOverride(name="longitude", column=@Column(name="lon"))
+    private Coordinates coordinates;
+
+    @Embedded
+    private PreferredTimeSlot preferredTimeSlot;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
