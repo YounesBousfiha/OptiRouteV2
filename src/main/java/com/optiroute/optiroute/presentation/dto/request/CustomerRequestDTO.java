408be66877d60dc30305fd9cfed3a11daa2c3ef7
Coderyounes <younesanas2021@gmail.com>
2025-11-06 12:28:06 +0100

    FEAT: add validation annotations to CustomerRequestDTO fields

--- src/main/java/com/optiroute/optiroute/presentation/dto/request/CustomerRequestDTO.java
+++ src/main/java/com/optiroute/optiroute/presentation/dto/request/CustomerRequestDTO.java
@@ -1,5 +1,8 @@
 package com.optiroute.optiroute.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Builder
@@ -7,15 +10,34 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class CustomerRequestDTO {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Street is required")
     private String street;
+
+    @NotBlank(message = "City is required")
     private String city;
+
+    @NotBlank(message = "Country is required")
     private String country;
+
     private String state;
+
+    @NotBlank(message = "Zip code is required")
     private String zipCode;
+
+    @NotNull(message = "Latitude is required")
     private Double latitude;
+
+    @NotNull(message = "Longitude is required")
     private Double longitude;
+
+    @Pattern(regexp = "^([01]?\\d|2[0-3]):[0-5]\\d$", message = "Start time must be in HH:mm format")
     private String preferredStartTime;
+
+    @Pattern(regexp = "^([01]?\\d|2[0-3]):[0-5]\\d$", message = "Start time must be in HH:mm format")
     private String preferredEndTime;
 }
