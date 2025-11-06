d2532cbfcaf44d3c77c4a4d03f926b318ad2c616
Coderyounes <younesanas2021@gmail.com>
2025-11-06 15:19:59 +0100

    FEAT: implement CRUD operations in CustomerController for customer management

--- src/main/java/com/optiroute/optiroute/presentation/controller/CustomerController.java
+++ src/main/java/com/optiroute/optiroute/presentation/controller/CustomerController.java
@@ -2,9 +2,16 @@ package com.optiroute.optiroute.presentation.controller;
 
 
 import com.optiroute.optiroute.application.service.CustomerService;
+import com.optiroute.optiroute.presentation.dto.request.CustomerRequestDTO;
+import com.optiroute.optiroute.presentation.dto.request.CustomerUpdateDTO;
+import com.optiroute.optiroute.presentation.dto.response.CustomerResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -17,10 +24,38 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    // create
-    // getAll
-    // getOne
-    // update
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(CustomerRequestDTO customerRequestDTO) {
+        CustomerResponseDTO customerResponseDTO = this.customerService.createCustomer(customerRequestDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(customerResponseDTO.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(customerResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
+        List<CustomerResponseDTO> customerResponseDTOS = this.customerService.getAllCustomers();
+        return ResponseEntity.ok(customerResponseDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable("id") Long id) {
+        CustomerResponseDTO customerResponseDTO = this.customerService.getOneCustomer(id);
+        return ResponseEntity.ok(customerResponseDTO);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable("id") Long id, @RequestBody CustomerUpdateDTO customerUpdateDTO) {
+        CustomerResponseDTO customerResponseDTO = this.customerService.updateCustomer(customerUpdateDTO, id);
+        return ResponseEntity.ok(customerResponseDTO);
+    }
     // delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") Long id) {
+        this.customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
