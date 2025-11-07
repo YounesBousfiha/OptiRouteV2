853eb3b60845b8eac591bbf3c32698b61e5f5e74
Coderyounes <younesanas2021@gmail.com>
2025-11-07 09:56:22 +0100

    FEAT: add DeliveryHistory entity for tracking delivery details

---
+++ src/main/java/com/optiroute/optiroute/domain/entity/DeliveryHistory.java
@@ -0,0 +1,38 @@
+package com.optiroute.optiroute.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "delivery_history")
+public class DeliveryHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "delivery_date", nullable = false)
+    private LocalDate deliveryDate;
+
+    @Column(name = "planned_delivery_time", nullable = false)
+    private LocalTime plannedDeliveryTime;
+
+    @Column(name = "actual_delivery_time", nullable = false)
+    private LocalTime actualDeliveryTime;
+
+    @Column(name = "delay_in_minutes", nullable = false)
+    private Long delayInMinutes;
+
+    @Column(name = "day_of_week", nullable = false)
+    private DayOfWeek dayOfWeek;
+}
+
