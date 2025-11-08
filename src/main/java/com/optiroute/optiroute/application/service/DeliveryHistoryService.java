9238fe2acd3fe0e313b0205bf7790b75d4c04b4c
Coderyounes <younesanas2021@gmail.com>
2025-11-08 01:36:56 +0100

    FEAT: implement DeliveryHistoryService to handle tour completion events and save delivery history

---
+++ src/main/java/com/optiroute/optiroute/application/service/DeliveryHistoryService.java
@@ -0,0 +1,60 @@
+package com.optiroute.optiroute.application.service;
+
+import com.optiroute.optiroute.domain.entity.Customer;
+import com.optiroute.optiroute.domain.entity.Delivery;
+import com.optiroute.optiroute.domain.entity.DeliveryHistory;
+import com.optiroute.optiroute.domain.entity.Tour;
+import com.optiroute.optiroute.domain.event.TourCompletedEvent;
+import com.optiroute.optiroute.domain.repository.DeliveryHistoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class DeliveryHistoryService {
+
+    private final DeliveryHistoryRepository deliveryHistoryRepository;
+
+
+    @Autowired
+    public DeliveryHistoryService(DeliveryHistoryRepository deliveryHistoryRepository) {
+        this.deliveryHistoryRepository = deliveryHistoryRepository;
+    }
+
+    @EventListener
+    @Async
+    public void handleTourCompleted(TourCompletedEvent event) {
+        Tour tour = event.getCompletedTour();
+
+        log.info("CONSUMER: Event Received for Tour ID : {}", tour.getId());
+
+        List<DeliveryHistory> deliveryHistoryList = new ArrayList<>();
+
+        for (Delivery delivery : tour.getDeliveryList()) {
+            LocalDateTime actual= LocalDateTime.now();
+            LocalDateTime planned = delivery.getPlannedDeliveryTime();
+            long delayInMinutes = Duration.between(planned, actual).toMinutes();
+
+            DeliveryHistory deliveryHistory = DeliveryHistory.builder()
+                    .delivery(delivery)
+                    .actualDeliveryTime(actual)
+                    .plannedDeliveryTime(planned)
+                    .delayInMinutes(delayInMinutes)
+                    .dayOfWeek(actual.getDayOfWeek())
+                    .build();
+
+            deliveryHistoryList.add(deliveryHistory);
+        }
+
+        deliveryHistoryRepository.saveAll(deliveryHistoryList);
+    }
+}
