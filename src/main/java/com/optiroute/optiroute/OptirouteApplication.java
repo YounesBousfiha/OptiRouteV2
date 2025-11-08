6072e23f62944089f4179a5c0ccf266406394864
Coderyounes <younesanas2021@gmail.com>
2025-11-08 01:37:34 +0100

    FEAT: enable asynchronous processing in OptirouteApplication

--- src/main/java/com/optiroute/optiroute/OptirouteApplication.java
+++ src/main/java/com/optiroute/optiroute/OptirouteApplication.java
@@ -1,13 +1,11 @@
 package com.optiroute.optiroute;
 
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class OptirouteApplication {
     public static void main(String[] args) {
         SpringApplication.run(OptirouteApplication.class, args);
