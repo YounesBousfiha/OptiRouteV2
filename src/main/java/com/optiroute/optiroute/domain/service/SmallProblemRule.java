10e23eef00cc4b55ba76cea4689908e8ac90e56c
Coderyounes <younesanas2021@gmail.com>
2025-11-08 01:37:48 +0100

    FEAT: update SmallProblemRule to allow larger delivery lists for NearestNeighborOptimizer

--- src/main/java/com/optiroute/optiroute/domain/service/SmallProblemRule.java
+++ src/main/java/com/optiroute/optiroute/domain/service/SmallProblemRule.java
@@ -7,12 +7,11 @@ import com.optiroute.optiroute.infrastructure.strategy.TourOptimizer;
 import java.util.List;
 import java.util.Optional;
 
-// TODO: add more Conditions Here
 public class SmallProblemRule implements OptimizerSelectionRule {
 
     @Override
     public Optional<TourOptimizer> getOptimizer(List<Delivery> deliveryList) {
-        if(!deliveryList.isEmpty() && deliveryList.size() < 20) {
+        if(!deliveryList.isEmpty() && deliveryList.size() <= 600) {
             return Optional.of(new NearestNeighborOptimizer());
         }
 
