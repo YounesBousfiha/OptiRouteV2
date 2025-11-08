57f59dce94e2ae6f66d8561ba2e359a32ab07bef
Coderyounes <younesanas2021@gmail.com>
2025-11-08 18:30:25 +0100

    FEAT: implement CommandLineRunner in OptirouteApplication for chat client interaction

--- src/main/java/com/optiroute/optiroute/OptirouteApplication.java
+++ src/main/java/com/optiroute/optiroute/OptirouteApplication.java
@@ -1,13 +1,37 @@
 package com.optiroute.optiroute;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@Slf4j
 @SpringBootApplication
 @EnableAsync
-public class OptirouteApplication {
+public class OptirouteApplication implements CommandLineRunner {
+
+
+    private final ChatClient chatClient;
+
+
+    @Autowired
+    public OptirouteApplication(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(OptirouteApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        String promptText = "Hello, what day is today ?";
+
+         String requestSpec = chatClient.prompt(promptText).call().content();
+
+         log.warn(requestSpec);
+    }
 }
