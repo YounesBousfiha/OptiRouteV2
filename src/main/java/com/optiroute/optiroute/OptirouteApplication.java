2a0430e6c1e1c0f1c20e9796d60b430d364e5734
Coderyounes <younesanas2021@gmail.com>
2025-11-09 21:16:11 +0100

    FEAT: remove CommandLineRunner implementation and associated ChatClient usage

--- src/main/java/com/optiroute/optiroute/OptirouteApplication.java
+++ src/main/java/com/optiroute/optiroute/OptirouteApplication.java
@@ -1,9 +1,6 @@
 package com.optiroute.optiroute;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,27 +8,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Slf4j
 @SpringBootApplication
 @EnableAsync
-public class OptirouteApplication implements CommandLineRunner {
-
-
-    private final ChatClient chatClient;
-
-
-    @Autowired
-    public OptirouteApplication(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
-    }
-
+public class OptirouteApplication {
     public static void main(String[] args) {
         SpringApplication.run(OptirouteApplication.class, args);
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        String promptText = "Hello, what day is today ?";
-
-         String requestSpec = chatClient.prompt(promptText).call().content();
-
-         log.warn(requestSpec);
-    }
 }
