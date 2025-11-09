c5bdb3d2842d9061d065188d3d0aea975beb46c9
Coderyounes <younesanas2021@gmail.com>
2025-11-09 16:33:20 +0100

    FEAT: refactor OptimizeFactory to use injected optimizers and add LargeProblemRule

--- src/main/java/com/optiroute/optiroute/application/service/OptimizeFactory.java
+++ src/main/java/com/optiroute/optiroute/application/service/OptimizeFactory.java
@@ -1,10 +1,15 @@
 package com.optiroute.optiroute.application.service;
 
 import com.optiroute.optiroute.domain.entity.Delivery;
+import com.optiroute.optiroute.domain.service.LargeProblemRule;
 import com.optiroute.optiroute.domain.service.MediumProblemRule;
 import com.optiroute.optiroute.domain.service.OptimizerSelectionRule;
 import com.optiroute.optiroute.domain.service.SmallProblemRule;
+import com.optiroute.optiroute.infrastructure.strategy.AIOptimizer;
+import com.optiroute.optiroute.infrastructure.strategy.ClarkeWrightOptimizer;
+import com.optiroute.optiroute.infrastructure.strategy.NearestNeighborOptimizer;
 import com.optiroute.optiroute.infrastructure.strategy.TourOptimizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,23 +19,34 @@ import java.util.Optional;
 public class OptimizeFactory {
 
     private final List<OptimizerSelectionRule> rules;
+    private final NearestNeighborOptimizer nearestNeighborOptimizer;
+    private final ClarkeWrightOptimizer clarkeWrightOptimizer;
+    private final AIOptimizer aiOptimizer;
 
-    public OptimizeFactory() {
+    @Autowired
+    public OptimizeFactory(
+            NearestNeighborOptimizer nearestNeighborOptimizer,
+            ClarkeWrightOptimizer clarkeWrightOptimizer,
+            AIOptimizer aiOptimizer
+    ) {
+
+        this.nearestNeighborOptimizer = nearestNeighborOptimizer;
+        this.clarkeWrightOptimizer = clarkeWrightOptimizer;
+        this.aiOptimizer = aiOptimizer;
         this.rules = List.of(
-                new SmallProblemRule(),
-                new MediumProblemRule()
-                // TODO: add AI rule Optimizer Here
+                new SmallProblemRule(nearestNeighborOptimizer),
+                new MediumProblemRule(clarkeWrightOptimizer),
+                new LargeProblemRule(aiOptimizer)
         );
     }
 
 
     public TourOptimizer createOptimize(List<Delivery> deliveryList) {
-        for(OptimizerSelectionRule rule: rules) {
-            Optional<TourOptimizer> optimizer = rule.getOptimizer(deliveryList);
-            if(optimizer.isPresent()) {
-                return optimizer.get();
-            }
-        }
-        return null;
+        return rules.stream()
+                .map(rule -> rule.getOptimizer(deliveryList))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .orElse(null);
     }
 }
